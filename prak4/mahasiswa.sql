0b8cd20651fd23c838cae854a390eec94de7f137
nadilapxl <108243353+nadiaalnd@users.noreply.github.com>
2023-03-30 13:13:27 +0700

    Update mahasiswa.sql
    Menambahkan data mahasiswa yang awalnya 5 menjadi 10

--- prak4/mahasiswa.sql
+++ prak4/mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2023 at 05:03 AM
+-- Generation Time: Mar 30, 2023 at 08:10 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `phpdasar`
+-- Database: `wpw`
 --
 
 -- --------------------------------------------------------
@@ -29,22 +29,32 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `mahasiswa` (
   `id` int(11) NOT NULL,
+  `nrp` char(20) DEFAULT NULL,
   `nama` varchar(100) DEFAULT NULL,
-  `nrp` char(10) DEFAULT NULL,
-  `jurusan` varchar(100) DEFAULT NULL,
+  `jurusan` varchar(50) DEFAULT NULL,
+  `gender` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
-  `gambar` varchar(100) DEFAULT NULL
+  `alamat` varchar(100) DEFAULT NULL,
+  `no_hp` char(20) DEFAULT NULL,
+  `asal_sma` varchar(50) DEFAULT NULL,
+  `matkul_fav` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `jurusan`, `email`, `gambar`) VALUES
-(1, 'Uzumaki Naruto', '40123301', 'Ilmu Komunikasi', 'naruto@gmail.com', 'naruto.jpeg'),
-(2, 'Hinata Hyuga', '40123302', 'Hubungan Internasional', 'hinata@gmail.com', 'hinata.jpeg'),
-(3, 'Nara Sikamaru', '40123303', 'Ilmu Politik', 'sikamaru@gmail.com', 'sikamaru.jpeg'),
-(4, 'Haruno Sakura', '40123304', 'Kedokteran', 'sakura@gmail.com', 'sakura.jpeg');
+INSERT INTO `mahasiswa` (`id`, `nrp`, `nama`, `jurusan`, `gender`, `email`, `alamat`, `no_hp`, `asal_sma`, `matkul_fav`) VALUES
+(1, '3122500001', 'Ade Hafis Rabbani', 'Teknik Informatika', 'laki-laki', 'hafis@it.student.pens.ac.id', 'Gresik', '085830556606', 'SMKN 1 Cerme', 'Matematika'),
+(2, '3122500002', 'Nadila Aulya Salsabila Mirdianti', 'Teknik Informatika', 'perempuan', 'nadila@it.student.pens.ac.id', 'Kediri', '082335995643', 'SMAN 1 Patianrowo', 'Workshop Pemrograman Web'),
+(3, '3122500003', 'Denti Widayati', 'Teknik Informatika', 'perempuan', 'denti@it.student.pens.ac.id', 'Sidoarjo', '089523304487', 'SMAN 3 Taruna Angkasa Madiun', 'Workshop Pemrograman Web'),
+(24, '3122500004', 'Zahrotul Hidayah', 'Teknik Informatika', 'perempuan', 'zahro@it.student.pens.ac.id', 'Lamongan', '085790342735', 'SMAN 1 Babat', 'Workshop Pemrograman Web'),
+(25, '3122500005', 'Gede Hari Yoga Nanda', 'Teknik Informatika', 'laki-laki', 'gedehari@it.student.pens.ac.id', 'Tulungagung', '083135368657', 'SMAN 1 Boyolangu', 'Workshop Pemrograman Web'),
+(29, '3122500006', 'Virgiawan Ivada Raksi Sekar Wibana', 'Teknik Informatika', 'laki-laki', 'virgiawan@it.student.pens.ac.id', 'Tuban', '085236113808', 'SMAN Jatirogo', 'Kewarganegaraan'),
+(30, '3122500007', 'Irfan Akmal Ardianto', 'Teknik Informatika', 'laki-laki', 'irfan@it.student.pens.ac.id', 'Palembang', '081274340490', 'SMA Plus Negeri 17 Palembang', 'Algoritma dan Struktur Data'),
+(31, '3122500008', 'Arsyita Devanaya Arianto', 'Teknik Informatika', 'perempuan', 'arsyita@it.student.pens.ac.id', 'Surabaya', '085171620044', 'SMAN 17 Surabaya', 'Algoritma dan Struktur Data'),
+(32, '3122500009', 'Mirta Chadhirotin Nachlah', 'Teknik Informatika', 'perempuan', 'mirta@it.student.pens.ac.id', 'Gresik', '089603443665', 'SMAN 1 Manyar', 'Workshop Pemrograman Web'),
+(33, '3122500010', 'Leody Zelvon Herliansa', 'Teknik Informatika', 'perempuan', 'leody@it.student.pens.ac.id', 'Tuban', '081331571335', 'SMAN 2 Tuban', 'Basis Data');
 
 --
 -- Indexes for dumped tables
@@ -64,7 +74,7 @@ ALTER TABLE `mahasiswa`
 -- AUTO_INCREMENT for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
